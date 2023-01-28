d02c7283531f0d14498987035df8a04de149b3ef
SergioNezhigay <abc1971h@gmail.com>
2023-01-28 22:31:52 +0200

    team in progress

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,30 +1,33 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-menu__close');
-    const $backdrop = document.querySelector('.header__backdrop');
-    const body = document.querySelector('body');
+  const mobileMenu = document.querySelector('.js-menu');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-menu__close');
+  const $backdrop = document.querySelector('.header__backdrop');
 
-    const toggleMenu = () => {
-      const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('open');
-      openMenuBtn.classList.toggle('open');
-      $backdrop.classList.toggle('hidden');
-      body.classList.toggle('scroll-disable');
-      // const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-      // bodyScrollLock[scrollLockMethod](document.body);
-    };
+  const body = document.querySelector('body');
 
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-  })();
-  
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('open');
+    openMenuBtn.classList.toggle('open');
+    $backdrop.classList.toggle('hidden');
+    body.classList.toggle('scroll-disable');
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
