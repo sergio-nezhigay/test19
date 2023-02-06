9f4fe4423e4c17a6030544c42702ef7b24ed128c
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-06 21:16:04 +0200

    111

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -22,11 +22,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  backdropEl.addEventListener('click', onbackdropClick);
-
-  const onbackdropClick = event => {
-    console.log(event);
-  };
+  backdropEl.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
