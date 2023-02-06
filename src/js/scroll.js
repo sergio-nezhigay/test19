7c9e21f723268e4b78f3a144ff98b1e239333b17
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-06 20:24:53 +0200

    11

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,8 +1,6 @@
 const arrowEl = document.querySelector('.hero__circle');
-console.log('start');
 
 arrowEl.addEventListener('click', () => {
-  console.log('clocked');
   document.querySelector('.advantages').scrollIntoView({
     behavior: 'smooth',
   });
