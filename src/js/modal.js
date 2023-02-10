39b53f7236d1e5c1b179c990d34e6d02697c1477
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-10 21:42:50 +0200

    final?

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,14 +2,19 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    body: document.querySelector('body'),
     modal: document.querySelector('[data-modal]'),
   };
-  console.log('modal started...');
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.body.addEventListener('click', onBodyClick);
 
   function toggleModal() {
-    console.log('toggleModal ...');
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('modal-open');
+  }
+
+  function onBodyClick(e) {
+    if (e.target.classList.contains('backdrop')) toggleModal();
   }
 })();
