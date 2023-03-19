2737fbb3fd7a14c4b496bbbf770dbc45df5b0921
sergio-nezhigay <58699786+sergio-nezhigay@users.noreply.github.com>
2023-03-19 15:43:48 +0000

    Deploying to gh-pages from @ sergio-nezhigay/test19@0a2e13a4fcc97a2f6f3b9090492fd590532cefdb 🚀

---
+++ index.9214aa3a.js
@@ -0,0 +1,2 @@
+var arrowEl=document.querySelector(".hero__circle");arrowEl.addEventListener("click",(function(){document.querySelector(".advantages").scrollIntoView({behavior:"smooth"})}));
+//# sourceMappingURL=index.9214aa3a.js.map
---
+++ services.cd60ac16.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),body:document.querySelector("body"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden"),e.body.classList.toggle("modal-open")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.body.addEventListener("click",(function(e){e.target.classList.contains("backdrop")&&o()}))}();
+//# sourceMappingURL=services.cd60ac16.js.map
