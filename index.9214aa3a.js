b3707886af120d45a2232f02218926831210cd6f
sergio-nezhigay <58699786+sergio-nezhigay@users.noreply.github.com>
2023-03-21 07:22:46 +0000

    Deploying to gh-pages from @ sergio-nezhigay/test19@f1872de86b404714a320a233f20f029b4dbb870d 🚀

--- index.9214aa3a.js
+++
@@ -1,2 +0,0 @@
-var arrowEl=document.querySelector(".hero__circle");arrowEl.addEventListener("click",(function(){document.querySelector(".advantages").scrollIntoView({behavior:"smooth"})}));
-//# sourceMappingURL=index.9214aa3a.js.map
