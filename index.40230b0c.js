c0ba32f7457ddcfae78255f4239b214ffb1a2927
sergio-nezhigay <58699786+sergio-nezhigay@users.noreply.github.com>
2023-04-01 18:33:38 +0000

    Deploying to gh-pages from @ sergio-nezhigay/test19@9416f9c27d1d98f2714600d7504563b34b892267 🚀

---
+++ index.40230b0c.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-menu"),document.querySelector(".js-open-menu"),document.querySelector(".js-menu__close"),document.querySelector(".header__backdrop"),document.querySelector("body");
+//# sourceMappingURL=index.40230b0c.js.map
---
+++ index.d841d818.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-menu"),document.querySelector(".js-open-menu"),document.querySelector(".js-menu__close"),document.querySelector(".header__backdrop"),document.querySelector("body");
+//# sourceMappingURL=index.d841d818.js.map
